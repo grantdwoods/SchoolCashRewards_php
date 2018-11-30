7a2602a630f34ef098da897b6daba324dcc2f143
GrantDwoods <grantdwoods@outlook.com>
2018-11-30 12:36:35 -0800

    Fixed GET and POST, stubbs for DELETE and PUT

--- sp_app/teachers.php
+++ sp_app/teachers.php
@@ -26,11 +26,12 @@ function filterOptions($claim)
 function getRequest($claim)
 {
   $userID = filter_input(INPUT_GET, 'userID');
+  $schoolID = $claim['schoolID'];
   
-  if($userID)
+  if($userID && $schoolID)
   {
-      $sql = 'SELECT * FROM tblteacher WHERE strTeacherID = ?';
-      $results = PDOexecuteQuery($sql,[$userID]);
+      $sql = 'SELECT * FROM tblteacher WHERE strTeacherID = ? AND intSchoolID = ?';
+      $results = PDOexecuteQuery($sql,[$userID, $schoolID]);
       if($results)
       {
         http_response_code (201);
@@ -39,7 +40,7 @@ function getRequest($claim)
       else
           http_response_code (500);
   }
-  elseif($claim['schoolID'])
+  elseif($schoolID)
   {
       $sql = 'SELECT * FROM tblteacher WHERE intSchoolID = ?';
       $results = PDOexecuteQuery($sql, [$claim['schoolID']]);
@@ -59,12 +60,12 @@ function postRequest($claim)
 {
     $schoolID = $claim['schoolID'];
     $userID = $claim['userID'];
-    $firstName = filter_input(INPUT_GET, 'firstName');
-    $lastName = filter_input(INPUT_GET, 'lastName');
+    $firstName = filter_input(INPUT_POST, 'firstName');
+    $lastName = filter_input(INPUT_POST, 'lastName');
     
     if($schoolID && $userID && $firstName && $lastName)
     {
-        $sql = 'INSTER INTO tblteacher (intSchoolID, strTeacherID, strFirstName, strLastName)'
+        $sql = 'INSERT INTO tblteacher (intSchoolID, strTeacherID, strFirstName, strLastName)'
                 . 'VALUES (?,?,?,?)';
         $results = PDOexecuteNonQuery($sql, [$schoolID, $userID, $firstName, $lastName]);
         if($results)
@@ -78,12 +79,16 @@ function postRequest($claim)
         http_response_code(400);
 }
 
-function putRequest()
+function putRequest($claim)
 {
-    
+    $str = file_get_contents('php://input');
+    $putVars = json_decode($str,true);
+    echo var_dump($putVars);
 }
 
-function deleteRequest()
+function deleteRequest($claim)
 {
-    
+    $str = file_get_contents('php://input');
+    $deleteVars = json_decode($str, true);
+    echo var_dump($deleteVars);
 }
