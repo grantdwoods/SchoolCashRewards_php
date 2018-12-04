d9a48a60ff7ec159bca24c56d1b0ccbf8c051cba
GrantDwoods <grantdwoods@outlook.com>
2018-12-03 17:08:13 -0800

    Fixed invalid method call in DBconn.

--- sp_app/classes.php
+++ sp_app/classes.php
@@ -1,8 +1,25 @@
 <?php
+include 'appDBconn.php';
+include 'validateRequest.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+validateRequest();
 
+function getRequest($claim)
+{
+
+}
+
+function postRequest($claim)
+{
+    
+}
+
+function putRequest($claim)
+{
+
+}
+
+function deleteRequest($claim)
+{
+   
+}
