0a9029d7dc042ec51c8ee541d1b0d762967fe8a8
GrantDwoods <grantdwoods@outlook.com>
2018-12-03 17:51:37 -0800

    put requests for teaches/takes

--- sp_app/takes.php
+++ sp_app/takes.php
@@ -11,12 +11,38 @@ function getRequest($claim)
 
 function postRequest($claim)
 {
-    
+    if(isset($_POST['userID']) && isset($_POST['classID']))
+    {
+        $userID = filter_input(INPUT_POST, 'userID');
+        $classID = filter_input(INPUT_POST, 'classID');
+        
+        $sql = 'INSERT INTO tblTakes (strStudentID, intClassID) VALUES (?,?)';
+        if(PDOexecuteNonQuery($sql,[$userID, $classID]))
+            http_response_code (201);
+        else
+            http_response_code (500);
+    }
+    else
+        http_response_code (400);
 }
 
 function putRequest($claim)
 {
-
+    $str = file_get_contents('php://input');
+    $putVars = json_decode($str,true);
+    if($putVars['userID'] && $putVars['classID'])
+    {
+        $sql = 'UPDATE tblTakes SET intClassID = ? WHERE strStudentID = ?';
+        if(PDOexecuteNonQuery($sql, [$putVars['classID'], $putVars['userID']]))
+            http_response_code (200);
+        else
+        {
+            http_response_code(200);
+            echo json_encode(array('err-message'=>'No changes.'));
+        }
+    }
+    else
+        http_response_code (400);
 }
 
 function deleteRequest($claim)
--- sp_app/teachers.php
+++ sp_app/teachers.php
@@ -42,7 +42,11 @@ function postRequest($claim)
             http_response_code(201);
         }
         else
+        {
             http_response_code(500);
+            echo json_encode(array('err-message'=>'No data added.')); 
+        }
+            
     }
     else
         http_response_code(400);
@@ -52,7 +56,19 @@ function putRequest($claim)
 {
     $str = file_get_contents('php://input');
     $putVars = json_decode($str,true);
-    echo var_dump($putVars);
+    if($putVars['userID'] && $putVars['classID'])
+    {
+        $sql = 'UPDATE tblTeaches SET intClassID = ? WHERE strTeacherID = ?';
+        if(PDOexecuteNonQuery($sql, [$putVars['classID'], $putVars['userID']]))
+            http_response_code (200);
+        else
+        {
+            http_response_code(200);
+            echo json_encode(array('err-message'=>'No changes.'));
+        }
+    }
+    else
+        http_response_code (400);
 }
 
 function deleteRequest($claim)
