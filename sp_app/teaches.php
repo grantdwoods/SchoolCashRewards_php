0304561aad0d961454b15f27875a7b2ade5ab7d3
GrantDwoods <grantdwoods@outlook.com>
2018-12-05 13:37:57 -0800

    Delete request for teaches.

--- sp_app/teaches.php
+++ sp_app/teaches.php
@@ -53,7 +53,7 @@ function putRequest($claim)
 {
     $str = file_get_contents('php://input');
     $putVars = json_decode($str,true);
-    if($putVars['userID'] && $putVars['classID'])
+    if(isset($putVars['userID']) && isset($putVars['classID']))
     {
         $sql = 'UPDATE tblteaches SET intClassID = ? WHERE strTeacherID = ?';
         if(PDOexecuteNonQuery($sql, [$putVars['classID'], $putVars['userID']]))
@@ -70,5 +70,38 @@ function putRequest($claim)
 
 function deleteRequest($claim)
 {
-   
+    $str = file_get_contents('php://input');
+    $deleteVars = json_decode($str,true);
+    $sql = 'DELETE FROM tblteaches WHERE';
+    $deleteTeacher = isset($deleteVars['userID']);
+    $deleteClass = isset($deleteVars['classID']);
+
+    if($deleteTeacher && $deleteClass)
+    {
+        $sql .= ' strTeacherID = ? AND intClassID = ?';
+        deleteData($sql,[$deleteVars['userID'],$deleteVars['classID']]);
+    } 
+    elseif(!$deleteTeacher && $deleteClass)
+    {
+        $sql .= ' intClassID = ?';
+        deleteData($sql, [$deleteVars['classID']]);
+    }
+    elseif($deleteTeacher && !$deleteClass)
+    {
+        $sql .= ' strTeacherID = ?';
+        deleteData($sql, [$deleteVars['userID']]);
+    }
+    else
+        http_response_code (400);
+}
+
+function deleteData($sql,$sqlVars)
+{
+    if(PDOexecuteNonQuery($sql, $sqlVars))
+        http_response_code (200);
+    else
+    {
+        http_response_code(200);
+        echo json_encode(array('err-message'=>'No changes.'));
+    }
 }
