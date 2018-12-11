792a19e08a4c370d693b2c6b2f60a459717a9fcc
GrantDwoods <grantdwoods@outlook.com>
2018-12-11 14:38:20 -0800

    Schools get requests.

--- sp_app/schools.php
+++ sp_app/schools.php
@@ -7,7 +7,18 @@ validateRequest();
 
 function getRequest($claim)
 {
-
+    $schoolID = '';
+    if(isset($_POST['schoolID']))
+        $schoolID = filter_input(INPUT_POST, 'schoolID', FILTER_SANITIZE_STRING);
+    else
+        $schoolID = $claim['schoolID'];
+    if($schoolID)
+    {
+        $sql = 'SELECT strSchoolName, strCashName from tblSchool WHERE intSchoolID = ?';
+        verifyGetResults(PDOexecuteQuery($sql, [$schoolID]));
+    }
+    else
+        http_response_code (400);
 }
 
 function postRequest($claim)
