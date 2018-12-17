407be4ad9ef98506a1aed26b55392e0d104eff97
GrantDwoods <grantdwoods@outlook.com>
2018-12-17 14:08:40 -0800

    Changed intClassCares to intClassCoupons, started +/- class coupons

--- sp_app/classes.php
+++ sp_app/classes.php
@@ -10,14 +10,14 @@ function getRequest($claim)
     if(isset($_GET['classID']))
     {
         $classID = filter_input(INPUT_GET, 'classID');
-        $sql = 'SELECT strClassName, intClassCares  FROM tblclass '
+        $sql = 'SELECT strClassName, intClassCoupons  FROM tblclass '
                 . 'WHERE intSchoolID = ? AND intClassID = ?';
         $results = PDOexecuteQuery($sql, [$claim['schoolID'], $classID]);
         verifyGetResults($results);
     }
     else
     {
-        $sql = 'SELECT strClassName, intClassCares FROM tblclass'
+        $sql = 'SELECT strClassName, intClassCoupons FROM tblclass'
                 . ' WHERE intSchoolID = ?';
         verifyGetResults(PDOexecuteQuery($sql, [$claim['schoolID']]));
     }
@@ -29,7 +29,10 @@ function postRequest($claim)
     $className = filter_input(INPUT_POST, 'className');
     if(isset($classID,$className))
     {
-        
+        $sql = 'INSERT INTO tblclass(intSchoolID, intClassID, strClassName, '
+                . 'intClassCoupons) VALUES (?,?,?,?)';
+        $varArray = [$claim['schoolID'], $classID, $className, 0];
+        verifyPostResults(PDOexecuteNonQuery($sql, $varArray));
     }
     else
         http_response_code (400);
@@ -37,10 +40,33 @@ function postRequest($claim)
 
 function putRequest($claim)
 {
-
+    $str = file_get_contents('php://input');
+    $putVars = json_decode($str, true);
+    if(isset($putVars['intClassID'], $putVars['coupons']))
+    {
+        
+    }
 }
 
 function deleteRequest($claim)
 {
    
+}
+
+function addCoupons($currentCount, $delta)
+{
+    $newCount = $currentCount + $delta;
+    if($newCount < 0)
+        $newCount = 0;
+    return $newCount;
+}
+
+function getCurrentCounponCount($classID, $schoolID)
+{
+    $sql = 'SELECT intClassCoupons FROM tblclass WHERE intclassID = ?'
+            . ' AND intSchoolID = ?';
+    $results = PDOexecuteQuery($sql, [$classID, $schoolID]);
+    if(isset($results[0]['intClassCoupons']))
+        return $results[0]['intClassCoupons'];
+    return null;
 }
