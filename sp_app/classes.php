9f79eec9203898e6dda1a5dd15c57a708741910e
GrantDwoods <grantdwoods@outlook.com>
2018-12-17 13:36:18 -0800

    Classes get, started post

--- sp_app/classes.php
+++ sp_app/classes.php
@@ -7,12 +7,32 @@ validateRequest();
 
 function getRequest($claim)
 {
-
+    if(isset($_GET['classID']))
+    {
+        $classID = filter_input(INPUT_GET, 'classID');
+        $sql = 'SELECT strClassName, intClassCares  FROM tblclass '
+                . 'WHERE intSchoolID = ? AND intClassID = ?';
+        $results = PDOexecuteQuery($sql, [$claim['schoolID'], $classID]);
+        verifyGetResults($results);
+    }
+    else
+    {
+        $sql = 'SELECT strClassName, intClassCares FROM tblclass'
+                . ' WHERE intSchoolID = ?';
+        verifyGetResults(PDOexecuteQuery($sql, [$claim['schoolID']]));
+    }
 }
 
 function postRequest($claim)
 {
-    
+    $classID = filter_input(INPUT_POST, 'classID');
+    $className = filter_input(INPUT_POST, 'className');
+    if(isset($classID,$className))
+    {
+        
+    }
+    else
+        http_response_code (400);
 }
 
 function putRequest($claim)
