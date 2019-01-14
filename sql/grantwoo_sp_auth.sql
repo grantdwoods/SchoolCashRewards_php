76321998c1a938314f97de76c2992038a85b1659
Grant Woods <grantdwoods@outlook.com>
2019-01-14 09:18:17 -0800

    Updated auth sql and postman.

--- sql/grantwoo_sp_auth.sql
+++ sql/grantwoo_sp_auth.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2018 at 10:38 PM
+-- Generation Time: Jan 14, 2019 at 06:17 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -32,23 +32,23 @@ CREATE TABLE `accounts` (
   `userID` varchar(30) COLLATE utf8_bin NOT NULL,
   `hash` text COLLATE utf8_bin NOT NULL,
   `role` char(1) COLLATE utf8_bin NOT NULL,
-  `schoolID` int(10) NOT NULL
+  `intSchoolID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`userID`, `hash`, `role`, `schoolID`) VALUES
+INSERT INTO `accounts` (`userID`, `hash`, `role`, `intSchoolID`) VALUES
 ('Bobby', '$2y$10$Lf.kAF0eQO0HJmhBRvonye5Qh9gPMKTENmi1PDAsB/.fh4bQdBd3S', 's', 1),
-('Jake', '$2y$10$9zkLd29/V1F.cCKazFAxe.Sb./EwlylTSPf0.laZwBkmqbLG6P4PG', 's', 1),
-('Jon', '$2y$10$AP8HBUsdhq7pntd8K6jGqeRDbsKwTpD5Lst6ARnYqL5dZF290RYAW', 'a', 2),
-('JonMan', '$2y$10$SygowpWOR7Mx2ee3ivuwDeuj.bA8potoDQDvJPGE6WzPeS44CXxp.', 't', 2),
+('Jack', '$2y$10$6PIFm1aXJkyDQAM.P8nMfeq3OBemnwISORw0VxqkOHvCyTksFP9GG', 's', 1),
+('Jane', '$2y$10$bS2q94Gez.3MZ61J1obTQ.uBllHqYnjBj7f0CEJFxMBISqzA0U0IS', 's', 1),
+('Jill', '$2y$10$ldoaNL/lyv/SMHuX7TpwtedaHf3wWrl8Q1bciGII/K7gxD8E8KJGW', 's', 1),
+('JonMan', '$2y$10$SygowpWOR7Mx2ee3ivuwDeuj.bA8potoDQDvJPGE6WzPeS44CXxp.', 'a', 2),
 ('Ryan', '$2y$10$SDNGs9c10L66JXMiA/qLi.pPY.81oqo8lH/p61/AdAnWUBqp/LYbu', 's', 1),
 ('Stan', '$2y$10$xSe30jGDnSgLdKL7OHqvsOw.LAX.H/mqF3zp6jxsNzGuUPHqHFJs2', 't', 1),
-('Tim', '$2y$10$tlodmSm.XS08wENr5Cp8FuAmitXMzm6E0qTOFd2bBiD33qC7tl/sK', 't', 1),
-('Timmy', '$2y$10$1qvS92J4kkIdiXKlmMmc5eiXlZHnFDUC76ryPnEq5SqVpABZvRoFe', 's', 1),
-('grant', '$2y$10$jVpBNDWol70NqwGY.q9PnuJf0K6KOeYGWZPNm3/p0bsaXfV9fIE.O', 'a', 1);
+('Tim', '$2y$10$tlodmSm.XS08wENr5Cp8FuAmitXMzm6E0qTOFd2bBiD33qC7tl/sK', 'a', 1),
+('grant', '$2y$10$ginOyfduDIXjgOKVl87z1OCXO8eHtiKrxPQ0iF4RRKjQR5RqvGeWi', 'a', 1);
 
 --
 -- Indexes for dumped tables
