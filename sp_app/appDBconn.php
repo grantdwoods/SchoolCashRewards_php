72b4c7540cdd8c868cddd3dca16597e5449bd9ab
Grant Woods <grantdwoods@outlook.com>
2019-03-03 15:12:52 -0800

    Allow PUT and DELETE

--- sp_app/appDBconn.php
+++ sp_app/appDBconn.php
@@ -87,7 +87,7 @@ function cors() {
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");         
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
