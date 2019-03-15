d742ca7f30501d8c9e6d55225d608eb45536aa41
Grant Woods <grantdwoods@outlook.com>
2019-03-15 14:36:20 -0700

    registering student

--- sp_auth/registerUser.php
+++ sp_auth/registerUser.php
@@ -24,6 +24,26 @@ else
    
 function registerStudent($claim){
     
+    $userID = filter_input(INPUT_POST, 'userID', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $classID = filter_input(INPUT_POST, 'classID', FILTER_SANITIZE_STRING);
+    if(isset($userID, $password)){
+        if(!checkForExistingUser){
+            if(addUser($userID, $password, "a", $claim['schoolID'])){
+                addStudentToClass($userID, $classID);
+                echo http_response_code(201);
+            }
+            echo http_response_code(400);
+        }
+        else
+        {
+            http_response_code(409);
+            echo json_encode(array('err-message'=>'User name taken.'));
+        }
+    }
+    else{
+        echo http_response_code(400);
+    }
 }
 
 function processPostRequest()
@@ -84,6 +104,11 @@ function addUser($userID,$passWord,$role,$schoolID)
     $hash = password_hash($passWord,PASSWORD_DEFAULT );
     $sql = 'INSERT INTO accounts (userID, hash, role, intSchoolID) VALUES (?,?,?,?)';
     return PDOexecuteNonQuery($sql,[$userID,$hash,$role,$schoolID]);
+}
+
+function addStudentToClass($userID, $classID){
+    $sql = 'INSERT INTO tbltakes (strStudentID, intClassID) VALUES (?,?)';
+    return PDOexecuteNonQuery($sql, [$userID, $classID]);
 }
 
 function checkForExistingSchool($schoolID)
